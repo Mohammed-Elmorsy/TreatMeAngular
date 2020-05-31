62a84cd6de478820a31c866b52fa37cff4aacf1b
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-05-31 05:25:40 +0200

    Changes in project structure

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/layout/header/header.component';
-import { FooterComponent } from './components/layout/footer/footer.component';
-
-
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [],
   imports: [
+
+  ],
+  exports:[
     CommonModule
   ]
 })
