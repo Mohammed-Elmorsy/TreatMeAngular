2fa4899a5d674aba38ab32475fff274493a20654
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-06-01 00:06:58 +0200

    Added models , services , components and assets

---
+++ src/app/_models/doctor.ts
@@ -0,0 +1,12 @@
+export class Doctor {
+    constructor(
+       public id:Number,       
+       public fullName:String,
+       public age:Number,
+       public address:String,
+       public speciality:String,
+       public email?:String
+       
+    )
+    {}
+}
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from "../shared/shared.module";
+import { HomeComponent } from './home.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    SharedModule,
+    RouterModule
+  ]
+})
+export class HomeModule { }
