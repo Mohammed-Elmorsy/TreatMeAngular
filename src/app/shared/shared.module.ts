1b3b4864eba8db96387a8b8976b064f18b592362
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-05-31 05:50:17 +0200

    sss

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/layout/header/header.component';
-import { FooterComponent } from './components/layout/footer/footer.component';
-
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
