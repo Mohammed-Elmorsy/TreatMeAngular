add0025eaa42dac38b99e1576aa497c1bd4d4f53
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-06-02 20:17:51 +0200

    Added empty doctor-list component with routes and Changes in home page styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { DoctorService } from '../core/services/doctor/doctor.service';
 
 import { Speciality } from '../_models/speciality';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +13,19 @@ import { Speciality } from '../_models/speciality';
 export class HomeComponent implements OnInit {
 
   specialities:Speciality[];
-  constructor(private doctorService:DoctorService) { }
+  constructor(private doctorService:DoctorService, 
+    private router:Router) { }
 
 
   ngOnInit() {
-    this.doctorService.getSpecialities()
-                            .subscribe(
-                              (_specialities)=> this.specialities = _specialities
-                              );
+    this.doctorService.getSpecialities().subscribe((_specialities)=>
+     this.specialities = _specialities );
+                             
     console.log(this.specialities);
+  }
+
+  navigateToDocsList(specialityID:number){
+    this.router.navigate(["doctor/list", specialityID])
   }
   
 
