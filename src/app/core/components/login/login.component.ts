8f7261a861675fb0654f5cbd9883bbf1376febfc
Mohammed Elmorsy <mohammed.elsayed.elmorsy>
2020-06-07 04:49:28 +0200

    Prepared UI of doctor-registeration ,patient-registeration and login

---
+++ src/app/core/components/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  pictureName:string="patient"
+
+  constructor() { 
+
+  }  
+
+  ngOnInit() {
+    
+  }
+  
+  
+  
+
+}
